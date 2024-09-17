4f489ac6fc47630c4c263234342f5d830e2a79cb
tanapp <tann2682004@gmail.com>
2024-09-17 13:24:52 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vũ Thiện Tân-BIT220247</Text>
       <StatusBar style="auto" />
     </View>
   );
